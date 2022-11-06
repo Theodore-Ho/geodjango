ec6e203911a740ae85896960bb6ac3be2113ba09
yuhong <heyuhong1273@gmail.com>
2022-11-06 17:27:48 +0000

    update profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 # accounts/urls.py
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import SignUpView
+from .views import profile
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup")
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("profile/", profile, name='profile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,30 @@
 # accounts/views.py
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
+
+from accounts.forms import UserUpdateForm
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, 'Your Profile has been updated!')
+            return redirect('home')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    context = {'u_form': u_form}
+    return render(request, 'registration/profile.html', context)
