e220840ba5dfbb10db2afaed8a01944afef9c0c3
yuhong <heyuhong1273@gmail.com>
2022-11-06 18:16:38 +0000

    change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 # accounts/urls.py
 from django.urls import path
-from django.views.generic import TemplateView
 
-from .views import SignUpView
-from .views import profile
+from .views import SignUpView, profile, change_password
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("profile/", profile, name='profile')
+    path("profile/", profile, name='profile'),
+    path("password/", change_password, name='password')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 # accounts/views.py
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -21,10 +22,26 @@ def profile(request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         if u_form.is_valid():
             u_form.save()
-            messages.success(request, 'Your Profile has been updated!')
             return redirect('home')
     else:
         u_form = UserUpdateForm(instance=request.user)
 
     context = {'u_form': u_form}
     return render(request, 'registration/profile.html', context)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        p_form = PasswordChangeForm(request.user, request.POST)
+        if p_form.is_valid():
+            user = p_form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        p_form = PasswordChangeForm(request.user)
+    return render(request, 'registration/password.html', {
+        'p_form': p_form
+    })
